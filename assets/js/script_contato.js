8f8c0520d991c4399db76c0f769d2b0b478bc2c0
Raphaela Munhão <79518019+raphamuhao@users.noreply.github.com>
2024-11-20 18:01:33 -0300

    Add files via upload

---
+++ assets/js/script_contato.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict'
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(forms)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          form.classList.add('was-validated')
+        } else {
+          inserir()
+          form.classList.remove('was-validated')
+          form.reset()
+        }
+        event.preventDefault()
+        event.stopPropagation()
+      }, false)
+    })
+})()
+
+const tel = document.getElementById('telefone');
+
+tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value))
+tel.addEventListener('change', (e) => mascaraTelefone(e.target.value))
+
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "")
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
+    tel.value = valor
+}
